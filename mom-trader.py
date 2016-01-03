f2a1dec7845167fabac8bb6fad117cb2077f8e6c
Luke <lkavenagh@gmail.com>
2016-01-03 08:36:51 -0800

    production ready

--- mom-trader.py
+++ mom-trader.py
@@ -9,7 +9,7 @@
 #
 #   easy_install requests
 
-import json, hmac, hashlib, time, requests, base64, config, sys
+import json, hmac, hashlib, time, requests, base64, config, sys, smtplib
 from datetime import datetime
 from dateutil import tz
 from requests.auth import AuthBase
@@ -18,14 +18,35 @@ requests.packages.urllib3.disable_warnings()
 
 LAST_N_TRADES = 50
 DOUBLE_DOWN_TOL = 2.0
+SELL_PERC_TRADES = 0.8
+BUY_PERC_TRADES = 0.5
+TURNAROUND_TRADES = 10
+TRADE_ATTEMPTS = 20
 QTY = 0.01
-
-print('   ')
 
 lastTrade = {'side': 'sell', 'size': 0, 'price': 0}
 
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('America/Los_Angeles')
+
+def sendEmail(subject, body):
+    msg = "\r\n".join([
+      "From: lkavenagh@gmail.com",
+      "To: lkavenagh@gmail.com",
+      "Subject: " + subject,
+      "",
+      body
+      ])
+    fromaddr = 'lkavenagh@gmail.com'
+    toaddrs = 'lkavenagh@gmail.com'
+    username = 'lkavenagh@gmail.com'
+    password = config.settings['gmailPwd']
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
 
 def getRecentTrades(N, auth):
     r = requests.get(api_url + 'products/BTC-USD/trades', auth=auth)
@@ -63,7 +84,7 @@ def lastXHrsPerf(X, auth):
     print('Avg. buy price: ' + str(round(buyCost,2)))
     print('Avg. sell price: ' + str(round(sellCost,2)))
 
-    print('Total trading profit: ' + str(round((sellCost*sellQty) - (buyCost*buyQty) - fees)))
+    print('Total trading profit: ' + str(round((sellCost*sellQty) - (buyCost*buyQty) - fees, 2)))
 
 def buy(qty, limit, auth):
     order = {
@@ -72,10 +93,8 @@ def buy(qty, limit, auth):
         'side': 'buy',
         'product_id': 'BTC-USD',
     }
-    #r = requests.post(api_url + 'orders', json=order, auth=auth)
-    print(str(datetime.now()) + '\nCreated BUY order:')
-    #print(json.dumps(r.json(), indent=4, sort_keys=True))
-    print('BOUGHT ' + str(qty) + 'BTC @ $' + str(limit) + "\n")
+    r = requests.post(api_url + 'orders', json=order, auth=auth)
+    return(r.json())
 
 
 def sell(qty, limit, auth):
@@ -85,10 +104,59 @@ def sell(qty, limit, auth):
         'side': 'sell',
         'product_id': 'BTC-USD',
     }
-    #r = requests.post(api_url + 'orders', json=order, auth=auth)
-    print(str(datetime.now()) + '\nCreated SELL order:')
-    #print(json.dumps(r.json(), indent=4, sort_keys=True))
-    print('SOLD ' + str(qty) + 'BTC @ $' + str(limit) + "\n")
+    r = requests.post(api_url + 'orders', json=order, auth=auth)
+    return(r.json())
+
+def sellLimit(qty, limit, auth):
+    print('SELL ' + str(qty) + 'BTC @ $' + str(limit) + "\n")
+    attempts = 0
+    r = sell(qty, limit, auth)
+    time.sleep(5)
+    status = requests.get(api_url + 'orders/' + r['id'], auth=auth).json()
+
+    while status['status'] != 'done' and attempts < TRADE_ATTEMPTS-1:
+        attempts += 1
+        requests.delete(api_url + 'orders/' + r['id'], auth=auth)
+        time.sleep(1)
+        book = getOrderBook(auth)
+        r = sell(qty, max(limit, float(book['asks'][0][0])), auth)
+        time.sleep(5)
+        status = requests.get(api_url + 'orders/' + r['id'], auth=auth).json()
+
+    if status['status'] == 'done':
+        print(datetime.now())
+        print('SOLD ' + str(status['size']) + 'BTC @ $' + str(status['price']) + "\n")
+        sendEmail('SOLD ' + str(status['size']) + 'BTC @ $' + str(status['price']), '')
+        return(True)
+    else:
+        print('SELL order not filled, cancelling')
+        cancelAll(auth)
+        return(False)
+
+def buyLimit(qty, limit, auth):
+    print('BUY ' + str(qty) + 'BTC @ $' + str(limit) + "\n")
+    attempts = 0
+    r = buy(qty, limit, auth)
+    time.sleep(5)
+    status = requests.get(api_url + 'orders/' + r['id'], auth=auth).json()
+    while status['status'] != 'done' and attempts < TRADE_ATTEMPTS-1:
+        attempts += 1
+        requests.delete(api_url + 'orders/' + r['id'], auth=auth)
+        time.sleep(1)
+        book = getOrderBook(auth)
+        r = buy(qty, min(limit, float(book['bids'][0][0])), auth)
+        time.sleep(5)
+        status = requests.get(api_url + 'orders/' + r['id'], auth=auth).json()
+
+    if status['status'] == 'done':
+        print(datetime.now())
+        print('BOUGHT ' + str(status['size']) + 'BTC @ $' + str(status['price']) + "\n")
+        sendEmail('BOUGHT ' + str(status['size']) + 'BTC @ $' + str(status['price']), '')
+        return(True)
+    else:
+        print('BUY order not filled, cancelling')
+        cancelAll(auth)
+        return(False)
 
 def cancelAll(auth):
     r = requests.delete(api_url + 'orders', auth=auth)
@@ -102,23 +170,23 @@ def getMyOrders(auth):
 def decideSide(trades, book, usdBalance, btcBalance, lastTrade, auth):
     lastMarketTrade = trades[0]['side']
     tradeHist = []
-    for i in range(1,LAST_N_TRADES):
+    for i in range(0,LAST_N_TRADES):
         if trades[i]['side'] == 'sell':
             tradeHist.append(1)
         else:
             tradeHist.append(-1)
 
-    if sum(tradeHist) > (LAST_N_TRADES*0.8) and sum([tradeHist[i] for i in range(0,10)]) < 10:
+    if sum(tradeHist) > (LAST_N_TRADES*SELL_PERC_TRADES) and sum([tradeHist[i] for i in range(0,TURNAROUND_TRADES)]) < TURNAROUND_TRADES:
         # Price is rising fast
         nextTrade = 'sell'
-    elif sum(tradeHist) < (-1*LAST_N_TRADES*0.5) and sum([tradeHist[i] for i in range(0,10)]) > -10:
+    elif sum(tradeHist) < (-1*LAST_N_TRADES*BUY_PERC_TRADES) and sum([tradeHist[i] for i in range(0,TURNAROUND_TRADES)]) > -TURNAROUND_TRADES:
         # Price is dropping fast
         nextTrade = 'buy'
     else:
         nextTrade = 'None'
 
-    sys.stdout.write('\r' + str(sum(tradeHist)))
-    sys.stdout.flush()
+    # sys.stdout.write('\r' + str(sum(tradeHist)))
+    # sys.stdout.flush()
 
     if nextTrade == 'None':
         return nextTrade
@@ -148,9 +216,6 @@ def decideSide(trades, book, usdBalance, btcBalance, lastTrade, auth):
             nextTrade = 'None'
         elif lastTrade['side'] == 'buy' and float(book['bids'][0][0]) > (lastTrade['price'] - DOUBLE_DOWN_TOL):
             nextTrade = 'None'
-
-    if (nextTrade == 'sell' and book['bids'][0][0] < lastTrade['price']):
-        nextTrade = 'None'
 
     return nextTrade
 
@@ -185,35 +250,43 @@ class CoinbaseExchangeAuth(AuthBase):
 api_url = config.settings['apiURL']
 auth = CoinbaseExchangeAuth(config.settings['apiKey'], config.settings['secret'], config.settings['passphrase'])
 
-# Get accounts
-r = requests.get(api_url + 'accounts', auth=auth).json()
-
-for account in r:
-    if account['currency'] == 'USD':
-        usdBalance = account['balance']
-    elif account['currency'] == 'BTC':
-        btcBalance = account['balance']
-
-print('USD/BTC balances: $' + usdBalance + ' / BTC' + btcBalance)
-
 try:
     while True:
+        # Get accounts
+        r = requests.get(api_url + 'accounts', auth=auth).json()
+
+        for account in r:
+            if account['currency'] == 'USD':
+                usdBalance = float(account['balance'])
+            elif account['currency'] == 'BTC':
+                btcBalance = float(account['balance'])
+
         trades = getRecentTrades(100, auth)
         book = getOrderBook(auth)
         tradeSide = decideSide(trades, book, usdBalance, btcBalance, lastTrade, auth)
         if tradeSide == 'buy':
             time.sleep(5)
             book = getOrderBook(auth)
-            buy(QTY, book['asks'][0][0], auth)
-            lastTrade = {'side': 'buy', 'size': QTY, 'price': book['asks'][0][0]}
+            success = buyLimit(QTY, book['bids'][0][0], auth)
+            if success:
+                lastTrade = {'side': 'buy', 'size': QTY, 'price': float(book['asks'][0][0])}
+                lastXHrsPerf(24, auth)
+                print('USD/BTC balances: $' + str(round(usdBalance,2)) + ' / BTC ' + str(round(btcBalance,4)))
+            else:
+                print('Trade not filled, returning to top.')
         elif tradeSide == 'sell':
             time.sleep(5)
             book = getOrderBook(auth)
-            sell(QTY, book['bids'][0][0], auth)
-            lastTrade = {'side': 'sell', 'size': QTY, 'price': book['bids'][0][0]}
-            lastXHrsPerf(24, auth)
+            success = sellLimit(QTY, book['asks'][0][0], auth)
+            if success:
+                lastTrade = {'side': 'sell', 'size': QTY, 'price': float(book['bids'][0][0])}
+                lastXHrsPerf(24, auth)
+                print('USD/BTC balances: $' + str(round(usdBalance,2)) + ' / BTC ' + str(round(btcBalance,4)))
+            else:
+                print('Trade not filled, returning to top.')
 
         time.sleep(2)
 except:
     cancelAll(auth)
+    sendEmail('Error occurred :(', '')
     raise
