c43dc14344440c4abd36d7a1dee4a951ff851256
Luke <lkavenagh@gmail.com>
2017-09-30 14:01:37 -0400

    .

--- config-safe.py
+++ config-safe.py
@@ -1,4 +1,9 @@
-api = {'apiKey':r'apikey',
-      'secret':r'secret',
-      'passphrase':r'passphrase'}
-          
+coinbase_api = {'apiKey':r'apikey',
+          'secret':r'secret',
+          'passphrase':r'pw',
+          'apiUrl':r'https://api.coinbase.com/v2/'}
+
+bittrex_api = {'apiKey':r'apikey',
+          'secret':r'secret',
+          'passphrase':r'pw',
+          'apiUrl':r'https://bittrex.com/api/v1.1/'}
