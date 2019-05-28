b5e0a867e3ca31d5d75e65e984a86dd7c2120b2d
Luke <lkavenagh@gmail.com>
2019-05-27 22:31:41 -0400

    .

--- config-safe.py
+++
@@ -1,9 +0,0 @@
-coinbase_api = {'apiKey':r'apikey',
-          'secret':r'secret',
-          'passphrase':r'pw',
-          'apiUrl':r'https://api.coinbase.com/v2/'}
-
-bittrex_api = {'apiKey':r'apikey',
-          'secret':r'secret',
-          'passphrase':r'pw',
-          'apiUrl':r'https://bittrex.com/api/v1.1/'}
