09bd058a6d2b0d42872ecff85b4cd4d0a0f15ca5
Luke <lkavenagh@gmail.com>
2021-01-30 12:47:45 -0500

    .

--- .ipynb_checkpoints/coinbase_algo-checkpoint.py
+++ .ipynb_checkpoints/coinbase_algo-checkpoint.py
@@ -11,8 +11,8 @@ chunk_size = 1
 lastBuyCost = trader.getBuyQuote('ETH', chunk_size)[0]
 lastSellProceeds = trader.getSellQuote('ETH', chunk_size)[0]
 
-print('{}: Last bought at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
-print('{}: Last sold at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
+sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
+sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
 
 newSellProceeds = newBuyProceeds = -1
 totalProfit = 0
@@ -22,7 +22,7 @@ while (newSellProceeds < 0) & (newBuyProceeds < 0):
         sell_quote = sell_quote[0]
         tmp = sell_quote - lastBuyCost
         if tmp > (newSellProceeds + abs(newSellProceeds)*0.01):
-            print('{}: ETH sell price (${:.2f}) rising: Sell proceeds would now be ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sell_quote, tmp))
+            sys.stdout.write('{}: ETH sell price (${:.2f}) rising: Sell proceeds would now be ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sell_quote, tmp))
         newSellProceeds = tmp
     
     buy_quote = trader.getBuyQuote('ETH', chunk_size)
@@ -30,27 +30,27 @@ while (newSellProceeds < 0) & (newBuyProceeds < 0):
         buy_quote = buy_quote[0]
         tmp = lastSellProceeds - buy_quote
         if tmp > (newBuyProceeds + abs(newBuyProceeds)*0.01):
-            print('{}: ETH buy price (${:.2f}) falling: Buy proceeds would now be ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), buy_quote, tmp))
+            sys.stdout.write('{}: ETH buy price (${:.2f}) falling: Buy proceeds would now be ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), buy_quote, tmp))
         newBuyProceeds = tmp
     
     if newSellProceeds > 0:
-        print('{}: Profitable trade!'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
-        print('{}: New sell generates ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newSellProceeds))
+        sys.stdout.write('{}: Profitable trade!\n'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
+        sys.stdout.write('{}: New sell generates ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newSellProceeds))
         totalProfit += newSellProceeds
         lastSellProceeds = sell_quote
         
-        print('{}: Last bought at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
-        print('{}: Last sold at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
+        sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
+        sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
         newSellProceeds = -1
         
     if newBuyProceeds > 0:
-        print('{}: Profitable trade!'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
-        print('{}: New buy would generate ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newBuyProceeds))
+        sys.stdout.write('{}: Profitable trade!\n'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
+        sys.stdout.write('{}: New buy would generate ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newBuyProceeds))
         totalProfit += newBuyProceeds
         lastBuyCost = buy_quote
         
-        print('{}: Last bought at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
-        print('{}: Last sold at ${:.2f}'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
+        sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
+        sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
         newBuyProceeds = -1
         
     time.sleep(5)
