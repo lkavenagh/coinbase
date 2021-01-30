37cedceefb12dd5ebf0828288d65d4df51b93492
Luke <lkavenagh@gmail.com>
2021-01-30 13:01:40 -0500

    .

--- coinbase_algo.py
+++ coinbase_algo.py
@@ -4,9 +4,6 @@ import time
 
 from Trader import Trader
 
-sys.stdout.write('Starting algo\n')
-sys.stdout.flush()
-
 trader = Trader()
 
 chunk_size = 1
@@ -14,34 +11,45 @@ chunk_size = 1
 lastBuyCost = trader.getBuyQuote('ETH', chunk_size)[0]
 lastSellProceeds = trader.getSellQuote('ETH', chunk_size)[0]
 
-sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
-sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
+sys.stdout.write('{}: First bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
+sys.stdout.write('{}: First sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
 sys.stdout.flush()
 
 newSellProceeds = newBuyProceeds = -1
+lastReportedSellPrice = lastReportedBuyPrice = 0
 totalProfit = 0
+totalUSDSpent = 0
 while (newSellProceeds < 0) & (newBuyProceeds < 0):
     sell_quote = trader.getSellQuote('ETH', chunk_size)
     if sell_quote is not None:
         sell_quote = sell_quote[0]
         tmp = sell_quote - lastBuyCost
-        if tmp > (newSellProceeds + abs(newSellProceeds)*0.01):
+        if tmp > (lastReportedSellPrice + abs(lastReportedSellPrice)*0.05):
             sys.stdout.write('{}: ETH sell price (${:.2f}) rising: Sell proceeds would now be ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), sell_quote, tmp))
+            lastReportedSellPrice = tmp
         newSellProceeds = tmp
     
     buy_quote = trader.getBuyQuote('ETH', chunk_size)
     if buy_quote is not None:
         buy_quote = buy_quote[0]
         tmp = lastSellProceeds - buy_quote
-        if tmp > (newBuyProceeds + abs(newBuyProceeds)*0.01):
+        if tmp > (lastReportedBuyPrice + abs(lastReportedBuyPrice)*0.05):
             sys.stdout.write('{}: ETH buy price (${:.2f}) falling: Buy proceeds would now be ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), buy_quote, tmp))
+            lastReportedBuyPrice = tmp
         newBuyProceeds = tmp
+    
+    sys.stdout.write('\n')
     
     if newSellProceeds > 0:
         sys.stdout.write('{}: Profitable trade!\n'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
-        sys.stdout.write('{}: New sell generates ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newSellProceeds))
+        sys.stdout.write('{}: New sell generates ${:.2f}\n\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newSellProceeds))
         totalProfit += newSellProceeds
         lastSellProceeds = sell_quote
+        lastReportedSellPrice = 0
+        
+        totalUSDSpent -= sell_quote
+        
+        sys.stdout.write('{}: Spent USD${:.2f} so far\n\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), totalUSDSpent))
         
         sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
         sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
@@ -49,9 +57,14 @@ while (newSellProceeds < 0) & (newBuyProceeds < 0):
         
     if newBuyProceeds > 0:
         sys.stdout.write('{}: Profitable trade!\n'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
-        sys.stdout.write('{}: New buy would generate ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newBuyProceeds))
+        sys.stdout.write('{}: New buy would generate ${:.2f}\n\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), newBuyProceeds))
         totalProfit += newBuyProceeds
         lastBuyCost = buy_quote
+        lastReportedBuyPrice = 0
+        
+        totalUSDSpent += buy_quote
+        
+        sys.stdout.write('{}: Spent USD${:.2f} so far\n\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), totalUSDSpent))
         
         sys.stdout.write('{}: Last bought at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastBuyCost))
         sys.stdout.write('{}: Last sold at ${:.2f}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S'), lastSellProceeds))
