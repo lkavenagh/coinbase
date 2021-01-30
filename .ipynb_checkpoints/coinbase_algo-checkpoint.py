58be1568c615c0df41c4536d19d1df20c3d44453
Luke <lkavenagh@gmail.com>
2021-01-30 18:26:10 -0500

    .

--- .ipynb_checkpoints/coinbase_algo-checkpoint.py
+++ .ipynb_checkpoints/coinbase_algo-checkpoint.py
@@ -101,6 +101,7 @@ sys.stdout.write('{}: Anchor price set at ${:.2f}\n'.format(time.strftime('%Y-%m
 sys.stdout.flush()
 
 sell_proceeds = buy_proceeds = -np.Inf
+global last_reported_sell_profit, last_reported_buy_profit
 last_reported_sell_profit = last_reported_buy_profit = -np.Inf
 
 total_profit = 0
